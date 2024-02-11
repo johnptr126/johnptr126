23456279ec71433c0df4607641296e3cdf1a2efb
johnptr126 <95537825+johnptr126@users.noreply.github.com>
2024-02-11 01:28:12 +0000

    Add files via upload

---
+++ Fate.js
@@ -0,0 +1,7 @@
+function autocastSpell() {
+  var grimoire = Game.Objects["Wizard tower"].minigame,
+      handFate = grimoire.spells["hand of fate"];
+  if (grimoire.magic == grimoire.magicM && grimoire.magic >= grimoire.getSpellCost(handFate))
+    grimoire.castSpell(handFate);
+}
+setInterval(function () { autocastSpell(); }, 1000);
