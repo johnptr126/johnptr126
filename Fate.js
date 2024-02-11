b3d693863059e058e695dd6f4b72ee1e04894fe5
johnptr126 <95537825+johnptr126@users.noreply.github.com>
2024-02-11 02:04:49 +0000

    Update Fate.js

--- Fate.js
+++ Fate.js
@@ -1,4 +1,4 @@
-function autocastSpell() {
+javascript:function autocastSpell() {
   var grimoire = Game.Objects["Wizard tower"].minigame,
       handFate = grimoire.spells["hand of fate"];
   if (grimoire.magic == grimoire.magicM && grimoire.magic >= grimoire.getSpellCost(handFate))
