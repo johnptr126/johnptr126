64c4e1357f4396995634b9922328242034b53489
johnptr126 <95537825+johnptr126@users.noreply.github.com>
2024-02-11 02:09:21 +0000

    Update Fate.js

--- Fate.js
+++ Fate.js
@@ -1,4 +1,6 @@
-javascript:function autocastSpell() {
+//Auto-Cast "Hand of Fate" at Max MP
+
+function autocastSpell() {
   var grimoire = Game.Objects["Wizard tower"].minigame,
       handFate = grimoire.spells["hand of fate"];
   if (grimoire.magic == grimoire.magicM && grimoire.magic >= grimoire.getSpellCost(handFate))
